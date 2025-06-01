added wds shuffle, removed temperature from sampling, fixed epoch calculation

nemo/collections/asr/data/audio_to_text.py
@@ -885,13 +885,14 @@ class _TarredAudioToTextDataset(IterableDataset):
             world_size=world_size,
             global_rank=global_rank,
         )
-
+        # print(audio_tar_filepaths)
         # Put together WebDataset pipeline
         self._dataset = wds.DataPipeline(
             wds.SimpleShardList(urls=audio_tar_filepaths),
             webdataset_split_by_workers,
-            wds.shuffle(shuffle_n),
+            wds.shuffle(len(audio_tar_filepaths)),
             wds.tarfile_to_samples(),
+            wds.shuffle(shuffle_n),
             wds.rename(audio=VALID_FILE_FORMATS, key='__key__'),
             wds.to_tuple('audio', 'key'),
             self._filter,
@@ -1384,7 +1385,6 @@ class RandomizedChainDataset(ChainDataset):
         self.rnd_gen = np.random.RandomState(rnd_seed)
 
     def __iter__(self):
-        print('HEREEEHEHE', len(self.datasets))
         shuffled_order = self.rnd_gen.permutation(len(self.datasets))
         for dataset_idx in shuffled_order:
             d = self.datasets[dataset_idx]

nemo/collections/common/data/dataset.py
@@ -79,7 +79,7 @@ class ConcatDataset(IterableDataset):
             self.index_generator = ConcatDataset.round_robin_generator
         else:
             raise ValueError(f"Currently we only support sampling techniques in {supported_sampling_techniques}.")
-        self.length = 0
+        self.length = 0 # inconcat dataset, one epoch would roughly mean 
 
         if isinstance(datasets[0], IterableDataset):
             self.kind = 'iterable'
@@ -94,7 +94,7 @@ class ConcatDataset(IterableDataset):
             if self.kind == 'map':
                 self.length += len(dataset) // world_size
             else:
-                self.length += len(dataset)
+                self.length += len(dataset) // world_size
 
         if self.sampling_scale != 1:
             self.length = int(self.length * self.sampling_scale)
@@ -163,15 +163,17 @@ class ConcatDataset(IterableDataset):
         np_rng = np.random.RandomState(seed)
         lengths = []
         num = len(datasets)
-        for dataset in datasets:
-            lengths.append(len(dataset))
+        print('SKIPPING CALCULATION')
+        # for dataset in datasets:
+        #     lengths.append(len(dataset))
 
-        p = np.array(lengths) / np.sum(lengths)
-        p = np.power(p, 1 / temp)
-        p = p / np.sum(p)
+        # p = np.array(lengths) / np.sum(lengths)
+        # p = np.power(p, 1 / temp)
+        # p = p / np.sum(p)
 
         while True:
-            ind = np_rng.choice(np.arange(num), p=p)
+            # ind = np_rng.choice(np.arange(num), p=p)
+            ind = np_rng.choice(np.arange(num))
             yield ind
 
     @staticmethod